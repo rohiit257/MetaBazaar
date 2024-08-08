7c8c139d83933d1539f342745fd00b73da5bbaa9
rohit <rohitshahi581@gmail.com>
2024-08-08 17:07:14 +0530

    Deployed To Sepolia Testnet
    $ npx hardhat ignition deploy ./ignition/modules/Token.js --network sepolia
    √ Confirm deploy to network sepolia (11155111)? ... yes
    Hardhat Ignition 🚀
    √ Confirm deploy to network sepolia (11155111)? ... yes
    Hardhat Ignition 🚀
    
    Deploying [ TokenModule ]
    Hardhat Ignition 🚀
    
    Deploying [ TokenModule ]
    
    Deploying [ TokenModule ]
    
    Batch #1
      Executed TokenModule#NFTSTORE
    
    [ TokenModule ] successfully deployed 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
+require("dotenv").config(); // Load environment variables
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
