b47d543fbb7e1cf0e6505cf0d942518273fe89fc
rohit <rohitshahi581@gmail.com>
2024-09-23 01:33:37 +0530

    ....

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+const __dirname = new URL('.', import.meta.url).pathname;
 const nextConfig = {
   images: {
     domains: [
@@ -27,6 +29,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+     ...config.resolve.alias,
+    '@': path.resolve(__dirname, './'),
+     };
+    return config;
+    },
 };
 
 export default nextConfig;
